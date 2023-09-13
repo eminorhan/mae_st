fdd704aa919acbb6222b2be72e594c5b3352fc3f
eminorhan <aeminorhan@gmail.com>
2023-09-13 00:03:10 -0400

    add training on adept starting from a pretrained model

--- scripts/train_maest_adept.sh
+++ scripts/train_maest_adept.sh
@@ -14,13 +14,38 @@ export MASTER_ADDR=$(hostname -s)
 export MASTER_PORT=$(shuf -i 10000-65500 -n 1)
 export WORLD_SIZE=1
 
-# vit-h/14
+# # vit-b/14 scratch
+# srun python -u ../main_pretrain.py \
+#     --path_to_data_dir /scratch/eo41/adept/videos/train \
+#     --save_prefix "adept_vitb14_224_sampling8_bs16_repeat2_pixloss_m09_accum1_Adam0001_predtdim16" \
+#     --output_dir ../models_maest \
+#     --model mae_vit_base_patch14 \
+#     --resume "" \
+#     --batch_size_per_gpu 16 \
+#     --accum_iter 1 \
+#     --epochs 100000 \
+#     --num_frames 16 \
+#     --input_size 224 \
+#     --decoder_embed_dim 512 \
+#     --decoder_depth 4 \
+#     --pin_mem \
+#     --num_workers 16 \
+#     --t_patch_size 2 \
+#     --repeat_aug 2 \
+#     --sampling_rate 8 \
+#     --lr 0.0001 \
+#     --weight_decay 0.05 \
+#     --mask_ratio 0.9 \
+#     --pred_t_dim 16 \
+#     --clip_grad 0.1
+
+# vit-h/14 pretrained
 srun python -u ../main_pretrain.py \
     --path_to_data_dir /scratch/eo41/adept/videos/train \
-    --save_prefix "adept_vitb14_224_sampling8_bs16_repeat2_pixloss_m09_accum1_Adam0001_predtdim16" \
+    --save_prefix "s+adept_vith14_224_sampling8_bs16_repeat1_pixloss_m09_accum1_Adam0001_predtdim16" \
     --output_dir ../models_maest \
-    --model mae_vit_base_patch14 \
-    --resume "" \
+    --model mae_vit_huge_patch14 \
+    --resume "../models_maest/s_vith14_224_8_1_16_normpixloss_m09_noamp_Adam0001.pth" \
     --batch_size_per_gpu 16 \
     --accum_iter 1 \
     --epochs 100000 \
@@ -31,7 +56,7 @@ srun python -u ../main_pretrain.py \
     --pin_mem \
     --num_workers 16 \
     --t_patch_size 2 \
-    --repeat_aug 2 \
+    --repeat_aug 1 \
     --sampling_rate 8 \
     --lr 0.0001 \
     --weight_decay 0.05 \
