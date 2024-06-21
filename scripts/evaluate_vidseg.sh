362d77889e65eeaeb37911efe80be23dde248829
eminorhan <aeminorhan@gmail.com>
2024-06-21 05:02:02 -0400

    add eval vidseg scoring

--- scripts/evaluate_vidseg.sh
+++ scripts/evaluate_vidseg.sh
@@ -5,16 +5,16 @@
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=240GB
-#SBATCH --time=01:10:00
+#SBATCH --time=03:00:00
 #SBATCH --job-name=evaluate_vidseg
 #SBATCH --output=evaluate_vidseg_%A_%a.out
 #SBATCH --array=0
 
 srun python -u ../evaluate_vidseg.py \
 	--data_path /vast/eo41/data/davis-2017/DAVIS \
-	--output_dir ../vidseg/kinetics \
-	--ckpt ../models_finetuned_imagenet/new/kinetics_imagenet_0.02.pth \
-	--img_size 224 \
-	--save_prefix kinetics
+	--output_dir ../vidseg/sayavakepicutego4d_448 \
+	--ckpt ../models_finetuned_imagenet/new/sayavakepicutego4d_448_imagenet_0.02.pth \
+	--img_size 448 \
+	--save_prefix sayavakepicutego4d_448
 
 echo "Done"
