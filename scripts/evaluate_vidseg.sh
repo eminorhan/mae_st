daad4df8704492dda70b4db542e442db4353a8a3
eminorhan <aeminorhan@gmail.com>
2024-06-21 02:57:27 -0400

    minor bug fix in evaluate_vidseg.sh

--- scripts/evaluate_vidseg.sh
+++ scripts/evaluate_vidseg.sh
@@ -13,7 +13,7 @@
 srun python -u ../evaluate_vidseg.py \
 	--data_path /vast/eo41/data/davis-2017/DAVIS \
 	--output_dir ../vidseg/kinetics \
-	--ckpt ../models_finetuned_imagenet/new/kinetics/kinetics_imagenet_0.02.pth \
+	--ckpt ../models_finetuned_imagenet/new/kinetics_imagenet_0.02.pth \
 	--img_size 224 \
 	--save_prefix kinetics
 
